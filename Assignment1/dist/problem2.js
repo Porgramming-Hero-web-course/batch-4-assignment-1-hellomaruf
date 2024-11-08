2349443393c5f6686177c7f1de0eaf66089b58db
maruf-ahmed <marufahamed1110@gmail.com>
2024-11-08 09:19:39 +0600

    solve problem 3

--- Assignment1/dist/problem2.js
+++ Assignment1/dist/problem2.js
@@ -1,4 +1,11 @@
 "use strict";
 {
-    console.log('hello');
+    // Create a TypeScript function removeDuplicates that accepts an array of numbers and returns a new array with duplicates removed. Preserve the original order of elements.
+    const removeDuplicates = (arr) => {
+        const withoutDuplicates = arr.filter((val, inx, array) => {
+            return array.indexOf(val) === inx;
+        });
+        return withoutDuplicates;
+    };
+    console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
 }
---
+++ Assignment1/src/problem3.ts
@@ -0,0 +1,15 @@
+{
+  // Create a TypeScript function called countWordOccurrences that accepts a sentence (string) and a word (string). The function should return the number of times the word appears in the sentence, ignoring case. Use the simple sentence without punctuation as input.
+  const countWordOccurrences = (sentence: string, word: string): number => {
+    const sentenceArray = sentence.toLowerCase().split(" ");
+    const providedWord = word.toLowerCase();
+    let countNum = 0;
+    sentenceArray.forEach((word) => {
+      if (word === providedWord) {
+        countNum++;
+      }
+    });
+    return countNum;
+  };
+  console.log(countWordOccurrences("Hello bro", "Bro"));
+}
