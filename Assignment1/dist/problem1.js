db9399af8ff2dd36fde1ab3e0804a9756a68a6e5
maruf-ahmed <marufahamed1110@gmail.com>
2024-11-08 08:41:26 +0600

    solve problem 2

--- Assignment1/dist/problem1.js
+++ Assignment1/dist/problem1.js
@@ -1,4 +1,9 @@
 "use strict";
 {
-    console.log('hello world');
+    // Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
+    const sumArray = (arr) => {
+        const sumOfArr = arr.reduce((acc, num) => acc + num, 0);
+        return sumOfArr;
+    };
+    //   console.log(sumArray([1, 2, 3, 4, 5]));
 }
